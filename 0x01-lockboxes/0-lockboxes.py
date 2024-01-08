8303b180931041463c0d52b63e9ebe9d3a3b7909
Adeosun Adeola <adeolaesther761@gmail.com>
2024-01-08 17:42:27 +0100

    interview_question

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A method that determines if all the boxes can be opened."""
+def canUnlockAll(boxes):
+    """Determines if all boxes can be unlocked."""
+    if len(boxes) == 0:
+        return True
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+    if len(keys) == len(boxes):
+        return True
+    return False
