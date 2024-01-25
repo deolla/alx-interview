871199d7d67161970bbced4847cadf187c5ce1ef
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-25 17:32:52 +0100

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,7 +4,16 @@ import sys
 import signal
 
 
+def print_stats(file_size, status_codes):
+    """Prints the stats"""
+    print("File size: {}".format(file_size))
+    for key in sorted(status_codes.keys()):
+        if status_codes[key] != 0:
+            print("{}: {}".format(key, status_codes[key]))
+
+
 if __name__ == "__main__":
+    file_size = 0
     status_codes = {
         "200": 0,
         "301": 0,
@@ -15,30 +24,18 @@ if __name__ == "__main__":
         "405": 0,
         "500": 0,
     }
-    file_size = 0
-    counter = 0
-
-    def print_stats():
-        """Prints the accumulated metrics"""
-        print("File size: {}".format(file_size))
-        for key, value in sorted(status_codes.items()):
-            if value != 0:
-                print("{}: {}".format(key, value))
 
     try:
-        for line in sys.stdin:
-            counter += 1
-            data = line.split()
+        for i, line in enumerate(sys.stdin, 1):
             try:
-                status = data[-2]
-                file_size += int(data[-1])
-                if status in status_codes:
-                    status_codes[status] += 1
-            except BaseException:
+                line = line.split()
+                file_size += int(line[-1])
+                status_codes[line[-2]] += 1
+            except Exception:
                 pass
-            if counter % 10 == 0:
-                print_stats()
+            if i % 10 == 0:
+                print_stats(file_size, status_codes)
     except KeyboardInterrupt:
-        print_stats()
+        print_stats(file_size, status_codes)
         raise
-    print_stats()
+    print_stats(file_size, status_codes)
