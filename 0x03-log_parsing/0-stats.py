ae9b629c44c6bab890062186331572d5c6b160e4
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2024-01-28 10:36:55 +0100

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-"""Log parsing module"""
-import sys
+"""Log parsing"""
 
 from sys import stdin
 
 try:
-    codes = {}
+    my_dict = {}
     total_size = 0
     for i, line in enumerate(stdin, start=1):
         parts = line.split(" ")
         try:
             total_size += int(parts[-1])
             status = int(parts[-2])
-            if status not in codes:
-                codes[status] = 1
+            if status not in my_dict:
+                my_dict[status] = 1
             else:
-                codes[status] += 1
+                my_dict[status] += 1
         except (ValueError, IndexError):
             continue
-        # codes = dict(sorted(codes.items()))
+
         if i % 10 == 0:
             print("File size: {}".format(total_size))
-            for key, val in sorted(codes.items()):
+            for key, val in sorted(my_dict.items()):
                 print("{}: {}".format(key, val))
 
 except KeyboardInterrupt:
@@ -29,5 +28,5 @@ except KeyboardInterrupt:
 
 finally:
     print("File size: {}".format(total_size))
-    for key, val in sorted(codes.items()):
+    for key, val in sorted(my_dict.items()):
         print("{}: {}".format(key, val))
