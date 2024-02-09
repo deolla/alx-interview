a24f21ad48d48cad23d9ce007d78f39c68f7d642
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-02-09 19:08:05 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,119 +1,49 @@
-#!/usr/bin/python3
-"""Write a program that solves the N queens problem."""
+#!/usr/bin/env python3
+"""N queens problem"""
 import sys
 
 
-def print_board(board):
-    """print_board
-    Args:
-        board - list of list with length sys.argv[1]
-    """
-    new_list = []
-    for i, row in enumerate(board):
-        value = []
-        for j, col in enumerate(row):
-            if col == 1:
-                value.append(i)
-                value.append(j)
-        new_list.append(value)
-
-    print(new_list)
-
-
-def isSafe(board, row, col, number):
-    """isSafe
-    Args:
-        board - list of list with length sys.argv[1]
-        row - row to check if is safe doing a movement in this position
-        col - col to check if is safe doing a movement in this position
-        number: size of the board
-    Return: True of False
-    """
-
-    # Check this row in the left side
-    for i in range(col):
-        if board[row][i] == 1:
+def is_safe(board, row, col, N):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
             return False
-
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, number, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
     return True
 
 
-def solveNQUtil(board, col, number):
-    """Auxiliar method to find the posibilities of answer
-    Args:
-        board - Board to resolve
-        col - Number of col
-        number - size of the board
-    Returns:
-        All the posibilites to solve the problem
-    """
+def solve_nqueens(board, row, N):
+    if row == N:
+        print_solution(board)
+        return
 
-    if col == number:
-        print_board(board)
-        return True
-    res = False
-    for i in range(number):
-
-        if isSafe(board, i, col, number):
-
-            # Place this queen in board[i][col]
-            board[i][col] = 1
-
-            # Make result true if any placement
-            # is possible
-            res = solveNQUtil(board, col + 1, number) or res
-
-            board[i][col] = 0  # BACKTRACK
-
-    return res
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row] = col
+            solve_nqueens(board, row + 1, N)
 
 
-def solve(number):
-    """Find all the posibilities if exists
-    Args:
-        number - size of the board
-    """
-    board = [[0 for i in range(number)] for i in range(number)]
-
-    if not solveNQUtil(board, 0, number):
-        return False
-
-    return True
+def print_solution(board):
+    print([[i, board[i]] for i in range(len(board))])
 
 
-def validate(args):
-    """Validate the input data to verify if the size to
-        answer is posible
-    Args:
-        args - sys.argv
-    """
-    if len(args) == 2:
-        # Validate data
-        try:
-            number = int(args[1])
-        except Exception:
-            print("N must be a number")
-            exit(1)
-        if number < 4:
-            print("N must be at least 4")
-            exit(1)
-        return number
-    else:
-        print("Usage: nqueens N")
-        exit(1)
+def nqueens(N):
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(N)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [0] * N
+    solve_nqueens(board, 0, N)
 
 
 if __name__ == "__main__":
-    """Main method to execute the application"""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-    number = validate(sys.argv)
-    solve(number)
+    nqueens(sys.argv[1])
