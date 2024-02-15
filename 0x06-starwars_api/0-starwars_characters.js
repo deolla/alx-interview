216fda13e3d4d600228db11c4da90671f1519ca0
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-02-15 22:08:20 +0100

    starwar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 // A script that prints all characters of a Star Wars movie
-const request = require("request");
-const url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, async function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    console.log("Response Body:", body); // Add this line for debugging
-
     try {
       const characters = JSON.parse(body).characters;
       for (const character of characters) {
@@ -19,7 +17,8 @@ request(url, async function (error, response, body) {
               reject(error);
             } else {
               try {
-                console.log(JSON.parse(body).name);
+                const characters = JSON.parse(body).name;
+                console.log(characters);
               } catch (parseError) {
                 console.error(parseError);
               }
