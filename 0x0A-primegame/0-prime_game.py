76e6811fd0d553c3ee39705a50c19a24af856e61
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-03-12 16:51:17 +0100

    prime_game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,54 +3,44 @@
 
 
 def isWinner(x, nums):
-    """Iswinner function."""
-    def is_prime(num):
-        """Is prime function"""
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
+    """Determines the winner of each game and returns the name of the player."""
+    def sieve(n):
+        """Implements the Sieve of Eratosthenes algorithm."""
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False
+        p = 2
+        while p * p <= n:
+            if primes[p]:
+                for i in range(p * p, n + 1, p):
+                    primes[i] = False
+            p += 1
+        return primes
 
-    def find_next_prime(nums):
-        """find next prime."""
-        for num in nums:
-            if is_prime(num):
-                return num
-        return None
-
-    def remove_multiples(nums, prime):
-        """Remove multiples function."""
-        return [num for num in nums if num % prime != 0]
+    def playGame(n):
+        """Simulates the game for a given value of n."""
+        primes = sieve(n)
+        turn = 0  # 0 for Maria, 1 for Ben
+        while True:
+            if all(not primes[i] for i in range(2, n + 1)):
+                return turn
+            for i in range(2, n + 1):
+                if primes[i]:
+                    for j in range(i, n + 1, i):
+                        primes[j] = False
+                    turn = 1 - turn
 
     maria_wins = 0
     ben_wins = 0
-
     for n in nums:
-        current_player = 'Maria'
-        game_nums = list(range(1, n + 1))
-
-        while True:
-            prime = find_next_prime(game_nums)
-
-            if prime is None:
-                break  # No prime numbers left
-
-            game_nums = remove_multiples(game_nums, prime)
-
-            if not game_nums:
-                if current_player == 'Maria':
-                    ben_wins += 1
-                else:
-                    maria_wins += 1
-                break
-
-            current_player = 'Ben' if current_player == 'Maria' else 'Maria'
+        winner = playGame(n)
+        if winner == 0:
+            maria_wins += 1
+        elif winner == 1:
+            ben_wins += 1
 
     if maria_wins > ben_wins:
-        return 'Maria'
+        return "Maria"
     elif ben_wins > maria_wins:
-        return 'Ben'
+        return "Ben"
     else:
         return None
