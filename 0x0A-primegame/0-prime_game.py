bd516d766e3f706a1cd935ec1fa7a125a9613b1c
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-03-12 16:43:25 +0100

    prime_game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,7 +3,9 @@
 
 
 def isWinner(x, nums):
+    """Iswinner function."""
     def is_prime(num):
+        """Is prime function"""
         if num < 2:
             return False
         for i in range(2, int(num**0.5) + 1):
@@ -12,12 +14,14 @@ def isWinner(x, nums):
         return True
 
     def find_next_prime(nums):
+        """find next prime."""
         for num in nums:
             if is_prime(num):
                 return num
         return None
 
     def remove_multiples(nums, prime):
+        """Remove multiples function."""
         return [num for num in nums if num % prime != 0]
 
     maria_wins = 0
